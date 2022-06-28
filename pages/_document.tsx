65a26083394b35d2ffda0ae9073c84a9c3e65718
Mohammad Khaki <mdkhaki10@gmail.com>
2022-06-29 01:19:28 +0430

    fix nextjs custom document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,22 @@
-import Document, {
-  DocumentContext,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-  render(): JSX.Element {
-    return (
-      <Html>
-        <Head>
-          <link rel="icon" href="/favicon.ico" type="image/ico" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
-            rel="stylesheet"
-          />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon.png" />
-          <meta name="theme-color" content="#fff" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/favicon.ico" type="image/ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <meta name="theme-color" content="#fff" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
